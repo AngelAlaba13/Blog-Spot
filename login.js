cefb3ab35a999bf41e8090752c9d5ab2e1f13480
Angel Kyle Alaba <angelkylealaba@gmail.com>
2024-06-15 18:10:21 +0800

    Initial commit

---
+++ login.js
@@ -0,0 +1,17 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('errorMessage');
+
+    if (username === 'admin' && password === '123456') {
+        alert('Welcome to my Blog Spot');
+        errorMessage.style.display = 'none';
+        window.location.href = 'contents/main.html';
+    } else {
+        errorMessage.textContent = 'Invalid username or password';
+        alert('username: admin \npassword: 123456');
+        errorMessage.style.display = 'block';
+    }
+});
